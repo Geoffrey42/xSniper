b5e5d082456c201ce7d707465782c4df2ab2bc18
Geoffrey GANE <geoffreygane@gmail.com>
2019-09-23 01:58:58 +0200

    refactor: add <common-key> to CSVFile constructor (#29)

--- tests/test_write_file.py
+++ tests/test_write_file.py
@@ -6,20 +6,22 @@ import unittest
 import pytest
 from unittest.mock import patch, mock_open
 from io import StringIO
-from xsniper.csv_file import CSVFile
+from .context import xsniper
 
 @pytest.fixture
 def set_csv_file():
     """Instantiate CSVFile object as a setup for every test."""
+
     in_memory_csv = StringIO("""\
 header1,header2,header3
 cell1,cell2,
 cell4,cell5,cell6""")
-    return CSVFile(in_memory_csv)
+    return in_memory_csv
 
 def test_file_opened_in_w_mode(set_csv_file):
     """Should have opened a file in write mode."""
-    got = set_csv_file
+
+    got = xsniper.CSVFile(set_csv_file, "cell1")
     fake_file_path = "fake/file/path"
     with patch('xsniper.csv_file.open', mock_open()) as mocked_file:
         got.write(fake_file_path)
@@ -28,7 +30,8 @@ def test_file_opened_in_w_mode(set_csv_file):
 
 def test_write_called_with_content(set_csv_file):
     """Should call write with content."""
-    got = set_csv_file
+
+    got = xsniper.CSVFile(set_csv_file, "cell1")
     fake_file_path = "fake/file/path"
     with patch('xsniper.csv_file.open', mock_open()) as mocked_file:
         got.write(fake_file_path)
