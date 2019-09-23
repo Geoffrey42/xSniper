6e810a307070e136f3c8966cc34f7721403adbdc
Geoffrey GANE <geoffreygane@gmail.com>
2019-09-23 02:01:57 +0200

    Revert "Revert "refactor: add <common-key> to CSVFile constructor (#29)" (#30)"
    This reverts commit 44f7b8fb837814a6a257ba340a49cf1e19df05f5.

--- tests/test_get_value.py
+++ tests/test_get_value.py
@@ -9,49 +9,54 @@ from .context import xsniper
 @pytest.fixture
 def set_csv_file():
     """Instantiate CSVFile object as a setup for every test."""
+
     in_memory_csv = StringIO("""\
 header1,header2,header3
 cell1,cell2,
 cell4,cell5,cell6
 cell7,,cell9""")
-    return xsniper.CSVFile(in_memory_csv)
+    return in_memory_csv
 
 def test_get_value(set_csv_file):
     """ Should return value from correct row and header """
-    target = set_csv_file
-    value = target.get_value("cell1", "header2")
+
+    target = xsniper.CSVFile(set_csv_file, "cell1")
+    value = target.get_value("header2")
 
     assert value == "cell2"
 
 def test_get_empty_value(set_csv_file):
     """ Should return empty value as well """
-    target = set_csv_file
-    value = target.get_value("cell7", "header2")
+
+    target = xsniper.CSVFile(set_csv_file, "cell7")
+    value = target.get_value("header2")
 
     assert value == ""
 
 def test_raise_if_empty_cell(set_csv_file):
     """ Should return from empty cell as well """
-    target = set_csv_file
+
+    target = xsniper.CSVFile(set_csv_file, "")
     with pytest.raises(ValueError):
-        target.get_value("", "header1")
+        target.get_value("header1")
 
 def test_raise_if_empty_header(set_csv_file):
     """ Should raise an exception if header argument is an empty string """
-    target = set_csv_file
+
+    target = xsniper.CSVFile(set_csv_file, "cell1")
     with pytest.raises(ValueError):
-        target.get_value("cell1", "")
+        target.get_value("")
 
 def test_raise_if_cell_arg_not_found(set_csv_file):
     """ Should raise an exception if cell argument is not found """
 
-    target = set_csv_file
+    target = xsniper.CSVFile(set_csv_file, "unknownCell")
     with pytest.raises(ValueError):
-        target.get_value("unknownCell", "header2")
+        target.get_value("header2")
 
 def test_raise_if_header_arg_not_found(set_csv_file):
     """ Should raise an exception if header argument is not found """
 
-    target = set_csv_file
+    target = xsniper.CSVFile(set_csv_file, "cell1")
     with pytest.raises(ValueError):
-        target.get_value("cell1", "unknownHeader")
+        target.get_value("unknownHeader")
